23c5f608b81254e4bead5a397d46e2e6a0ed2765
Yan Victor <61756965+KagoBoy@users.noreply.github.com>
2025-09-26 08:15:14 -0300

    sellerDaoJDBC e DaoFactory

---
+++ db/src/dao_impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package dao_impl;
+
+import java.util.List;
+
+import dao.SellerDao;
+import model.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void update(Seller obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+}
