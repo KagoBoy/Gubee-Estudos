1aeadb7c1fc98bf52f6ab39ade2273e9e3309d29
Yan Victor <61756965+KagoBoy@users.noreply.github.com>
2025-09-26 07:55:57 -0300

    first commit db

---
+++ db/src/App.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+// import java.util.Arrays;
+// import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+        try{
+            conn = db.getConnection();
+            st = conn.createStatement();
+
+            rs = st.executeQuery("select * from department");
+            
+            while(rs.next()){
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("name"));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally{
+            rs.close();
+            st.close();
+            db.closeConnection();
+        }
+    }
+}
