a39e8f594cc2771326b188fa1360280b991fe89d
Yan Victor <61756965+KagoBoy@users.noreply.github.com>
2025-09-29 11:41:10 -0300

    banco de dados h2 com jpa

--- first-spring-boot/src/main/java/com/example/first_spring_boot/entities/User.java
+++ first-spring-boot/src/main/java/com/example/first_spring_boot/entities/User.java
@@ -2,9 +2,21 @@ package com.example.first_spring_boot.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_user") //renomeando para nao dar conflito com o banco, a palavra User
 public class User implements Serializable{
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
