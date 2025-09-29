e82ea0f1e398bdb962977ab5556efc8c8f681036
Yan Victor <61756965+KagoBoy@users.noreply.github.com>
2025-09-29 12:06:41 -0300

    jpa repository, dependecy injection, database seeding

---
+++ first-spring-boot/src/main/java/com/example/first_spring_boot/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.first_spring_boot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.first_spring_boot.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
+
+//essa interface nao precisa ser implementada, pois já é implementada pelo
+//JPARepository quando é passado a o tipo da entidade e ID
