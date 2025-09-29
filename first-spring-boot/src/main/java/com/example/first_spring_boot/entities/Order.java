d6b2923b6336c2f4db5d30f172f7e88519d62c46
Yan Victor <61756965+KagoBoy@users.noreply.github.com>
2025-09-29 14:58:34 -0300

    orderStatus enum

--- first-spring-boot/src/main/java/com/example/first_spring_boot/entities/Order.java
+++ first-spring-boot/src/main/java/com/example/first_spring_boot/entities/Order.java
@@ -3,6 +3,7 @@ package com.example.first_spring_boot.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.example.first_spring_boot.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -26,6 +27,8 @@ public class Order implements Serializable{
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne //Annotation para informar que a relação com cliente é de muitos para um
     @JoinColumn(name = "client_id") //seta o nome da chave estrangeira no banco de dados
     private User client;
@@ -33,9 +36,10 @@ public class Order implements Serializable{
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -63,6 +67,18 @@ public class Order implements Serializable{
         this.client = client;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -88,6 +104,8 @@ public class Order implements Serializable{
         return true;
     }
 
+
+    
     
 
 }
