351d2f7c04e5b4d692054f3c981e68d1d3002f0d
Yan Victor <61756965+KagoBoy@users.noreply.github.com>
2025-10-03 10:25:30 -0300

    maven modularizacao dos projetos demo/first-spring-boot

--- demo/src/main/com/example/demo/programa/App.java
+++ demo/src/main/com/example/demo/programa/App.java
@@ -9,36 +9,38 @@ import com.example.demo.dominio.Pessoa;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Pessoa p1 = new Pessoa(null, "Teste123", "teste123@gmail.com");
-        // Pessoa p2 = new Pessoa(null, "Pessoa 2", "pessoa2@gmail.com");
-        // Pessoa p3 = new Pessoa(null, "Pessoa 3", "pessoa3@gmail.com");
+        Pessoa p1 = new Pessoa(null, "Teste123", "teste123@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Pessoa 2", "pessoa2@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Pessoa 3", "pessoa3@gmail.com");
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
+        
+
+        // Pessoa p = em.find(Pessoa.class, 2); //busca a pessoa na tabela pelo id
+        // System.out.println(p);
+
+        //Cria uma tabela pessoa e insere os dados no banco
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        System.out.println("Pronto!");
+
         //Para excluir, o objeto precisa estar monitorado
         //Ou seja, acabou de ser instanciado ou acabou de ser buscado com o .find()
-        Pessoa p = em.find(Pessoa.class, 2);
+        Pessoa p = em.find(Pessoa.class, p2.getId());
         em.getTransaction().begin();
         em.remove(p);
         em.getTransaction().commit();
 
         //É necessário o getTransaction().begin() e o .commit() sempre que não seja apenas consulta
 
-        // Pessoa p = em.find(Pessoa.class, 2); //busca a pessoa na tabela pelo id
-        // System.out.println(p);
-
-        //Cria uma tabela pessoa e insere os dados no banco
-        // em.getTransaction().begin();
-        // em.persist(p1);
-        // em.persist(p2);
-        // em.persist(p3);
-        // em.getTransaction().commit();
-        // System.out.println("Pronto!");
-
-        // System.out.println(p1);
-        // System.out.println(p2);
-        // System.out.println(p3);
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
         em.close();
         emf.close();
     }
