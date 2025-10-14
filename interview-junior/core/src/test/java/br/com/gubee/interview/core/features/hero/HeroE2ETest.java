bee8b7b9101bdfc98fbcd81397fca30650529773
Yan Victor <61756965+KagoBoy@users.noreply.github.com>
2025-10-14 13:51:20 -0300

    e2e test for updateHero

--- interview-junior/core/src/test/java/br/com/gubee/interview/core/features/hero/HeroE2ETest.java
+++ interview-junior/core/src/test/java/br/com/gubee/interview/core/features/hero/HeroE2ETest.java
@@ -1,6 +1,7 @@
 package br.com.gubee.interview.core.features.hero;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -11,18 +12,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
+import br.com.gubee.interview.core.features.powerstats.PowerStatsRepository;
 import br.com.gubee.interview.model.Hero;
+import br.com.gubee.interview.model.PowerStats;
 import br.com.gubee.interview.model.enums.Race;
 import br.com.gubee.interview.model.request.CreateHeroRequest;
 
@@ -31,13 +32,14 @@ import br.com.gubee.interview.model.request.CreateHeroRequest;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class HeroE2ETest {
 
-
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Autowired
     private HeroRepository heroRepository;
 
+    @Autowired
+    private PowerStatsRepository powerStatsRepository;
 
     @Test
     void shouldCreateHero_WhenValidRequest() {
@@ -56,7 +58,6 @@ public class HeroE2ETest {
                 request,
                 Void.class);
 
-
         assertEquals(response.getStatusCode(), HttpStatus.CREATED);
         assertNotNull(response.getHeaders().getLocation());
 
@@ -66,6 +67,77 @@ public class HeroE2ETest {
         Optional<Hero> savedHero = heroRepository.findById(UUID.fromString(heroId));
         assertTrue(savedHero.isPresent());
         assertEquals(savedHero.get().getName(), randomName);
+    }
+
+    @Test
+    void shouldUpdateHero_WhenValidRequest() {
+        String randomName = UUID.randomUUID().toString().replace("-", "");
+        CreateHeroRequest request = CreateHeroRequest.builder()
+                .name(randomName)
+                .race(Race.HUMAN)
+                .strength(6)
+                .agility(5)
+                .dexterity(8)
+                .intelligence(10)
+                .build();
+
+        ResponseEntity<Void> response = restTemplate.postForEntity(
+                "/api/v1/heroes",
+                request,
+                Void.class);
+
+        assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+        assertNotNull(response.getHeaders().getLocation());
+
+        String location = response.getHeaders().getLocation().toString();
+        String heroId = location.substring(location.lastIndexOf("/") + 1);
+        UUID heroUUID = UUID.fromString(heroId);
+
+        Optional<Hero> savedHero = heroRepository.findById(heroUUID);
+        assertTrue(savedHero.isPresent());
+        assertEquals(savedHero.get().getName(), randomName);
+        assertEquals(savedHero.get().getRace(), Race.HUMAN);
+
+        String updatedName = randomName + "_Updated";
+        CreateHeroRequest updateRequest = CreateHeroRequest.builder()
+                .name(updatedName)
+                .race(Race.DIVINE)
+                .strength(10)
+                .agility(10)
+                .dexterity(10)
+                .intelligence(10)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<CreateHeroRequest> updateEntity = new HttpEntity<>(updateRequest, headers);
+
+        ResponseEntity<Hero> updateResponse = restTemplate.exchange(
+                "/api/v1/heroes/update/" + heroId,
+                HttpMethod.PUT,
+                updateEntity,
+                Hero.class);
+
+        assertEquals(HttpStatus.OK, updateResponse.getStatusCode());
+        assertNotNull(updateResponse.getBody());
+
+        Hero updatedHero = updateResponse.getBody();
+        assertEquals(updatedName, updatedHero.getName());
+        assertEquals(Race.DIVINE, updatedHero.getRace());
+
+        Optional<Hero> heroFromDb = heroRepository.findById(heroUUID);
+        assertTrue(heroFromDb.isPresent());
+        assertEquals(updatedName, heroFromDb.get().getName());
+        assertEquals(Race.DIVINE, heroFromDb.get().getRace());
+
+        UUID powerStatsId = heroFromDb.get().getPowerStatsId();
+        PowerStats updatedPowerStats = powerStatsRepository.findById(powerStatsId);
+        assertFalse(updatedPowerStats.equals(null));
+        assertEquals((short) 10, updatedPowerStats.getStrength());
+        assertEquals((short) 10, updatedPowerStats.getAgility());
+        assertEquals((short) 10, updatedPowerStats.getDexterity());
+        assertEquals((short) 10, updatedPowerStats.getIntelligence());
+
     }
 
 }
