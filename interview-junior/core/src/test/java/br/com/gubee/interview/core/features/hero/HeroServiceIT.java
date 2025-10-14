f6d77b5722f089123243a9aee49d3baf7b0e4d76
Yan Victor <61756965+KagoBoy@users.noreply.github.com>
2025-10-14 08:52:46 -0300

    testes de integracao create and update tests

--- interview-junior/core/src/test/java/br/com/gubee/interview/core/features/hero/HeroServiceIT.java
+++ interview-junior/core/src/test/java/br/com/gubee/interview/core/features/hero/HeroServiceIT.java
@@ -7,9 +7,11 @@ import br.com.gubee.interview.model.enums.Race;
 import br.com.gubee.interview.model.request.CreateHeroRequest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -17,57 +19,94 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("it")
 public class HeroServiceIT {
 
-    @Mock
+    @Autowired
     private PowerStatsRepository powerStatsRepository;
 
-    @Mock
+    @Autowired
     private HeroRepository heroRepository;
 
     @Autowired
     private HeroService heroService;
 
+    @Transactional
     @Test
     public void createHeroWithAllRequiredArguments() {
 
-        UUID powerStatsId = UUID.randomUUID();
-        UUID heroId = UUID.randomUUID();
+        CreateHeroRequest request = createHeroRequest();
 
-        PowerStats powerStats = PowerStats.builder()
-                .strength(createHeroRequest().getStrength())
-                .agility(createHeroRequest().getAgility())
-                .dexterity(createHeroRequest().getDexterity())
-                .intelligence(createHeroRequest().getIntelligence())
-                .build();
+        UUID heroId = heroService.create(request);
 
-        Hero hero = Hero.builder()
-            .name(createHeroRequest().getName())
-            .race(createHeroRequest().getRace())
-            .powerStatsId(powerStatsId)
-            .build();
+        Optional<Hero> savedHero = heroRepository.findById(heroId);
+        assertTrue(savedHero.isPresent());
+        Hero hero = savedHero.get();
+        assertEquals(request.getName(), hero.getName());
+        assertEquals(request.getRace(), hero.getRace());
+
+        UUID powerStatsId = hero.getPowerStatsId();
+        PowerStats savedPowerStats = powerStatsRepository.findById(powerStatsId);
+        assertTrue(!savedPowerStats.equals(null));
+
+        assertEquals(request.getStrength(), savedPowerStats.getStrength());
+        assertEquals(request.getAgility(), savedPowerStats.getAgility());
+        assertEquals(request.getDexterity(), savedPowerStats.getDexterity());
+        assertEquals(request.getIntelligence(), savedPowerStats.getIntelligence());
+    }
+
+    @Transactional
+    @Test
+    public void updateHeroById() {
+
+        CreateHeroRequest request = createHeroRequest();
+        UUID heroId = heroService.create(request);
+        
+        CreateHeroRequest updateRequest = updateRequest();
+        heroService.updateById(updateRequest, heroId);
         
 
-        when(powerStatsRepository.create(any(PowerStats.class))).thenReturn(powerStatsId);
+        Optional<Hero> savedHero = heroRepository.findById(heroId);
+        assertTrue(savedHero.isPresent());
 
-        when(heroRepository.create(any(Hero.class))).thenReturn(heroId);
 
-        UUID result = heroService.create(createHeroRequest());
+        Hero updatedHero = savedHero.get();
 
-        assertEquals(heroId, result);
+        assertEquals(updateRequest.getName(), updatedHero.getName());
+        assertEquals(updateRequest.getRace(), updatedHero.getRace());
+
+        UUID powerStatsId = updatedHero.getPowerStatsId();
+        PowerStats updatedPowerStats = powerStatsRepository.findById(powerStatsId);
+        assertTrue(!updatedPowerStats.equals(null));
+
+        assertEquals(updateRequest.getStrength(), updatedPowerStats.getStrength());
+        assertEquals(updateRequest.getAgility(), updatedPowerStats.getAgility());
+        assertEquals(updateRequest.getDexterity(), updatedPowerStats.getDexterity());
+        assertEquals(updateRequest.getIntelligence(), updatedPowerStats.getIntelligence());
     }
 
     private CreateHeroRequest createHeroRequest() {
         return CreateHeroRequest.builder()
-            .name("Yan")
-            .agility(5)
-            .dexterity(8)
-            .strength(6)
-            .intelligence(10)
-            .race(Race.HUMAN)
-            .build();
+                .name("Aquaman")
+                .agility(5)
+                .dexterity(8)
+                .strength(6)
+                .intelligence(10)
+                .race(Race.HUMAN)
+                .build();
+    }
+
+    private CreateHeroRequest updateRequest() {
+        return CreateHeroRequest.builder()
+                .name("Wonder Woman")
+                .agility(8)
+                .dexterity(9)
+                .strength(7)
+                .intelligence(10)
+                .race(Race.ALIEN)
+                .build();
     }
 }
